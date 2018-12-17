8c01819a8ded57d1a6af264bb77fff9ba36fc968
maxild <mmaxild@gmail.com>
2018-12-17 20:16:37 +0100

    Added javascript to all pages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// on page load pseudo handler
 (function() {
     // page singleton elements
     let backdrop = document.querySelector(".backdrop");
@@ -7,6 +8,9 @@
     // console.dir(backdrop);
 
     let choosePlanButtons = document.querySelectorAll(".plan button");
+
+    let hamburgerButton = document.querySelector(".toggle-button");
+    let mobileNav = document.querySelector(".mobile-nav");
 
     for (let i = 0; i < choosePlanButtons.length; i++) {
         // arrow function does not work in IE
@@ -22,10 +26,18 @@
 
     // const requires IE11
     const closeModal = function() {
+        // spaghetti code...we just hide every modal and the backdrop
         modal.style.display = "none";
         backdrop.style.display = "none";
+        mobileNav.style.display = "none";
     };
 
     modalNoButton.addEventListener("click", closeModal);
+
     backdrop.addEventListener("click", closeModal);
+
+    hamburgerButton.addEventListener("click", function() {
+        mobileNav.style.display = "block";
+        backdrop.style.display = "block";
+    });
 })();
